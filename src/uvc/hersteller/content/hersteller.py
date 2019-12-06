f3ba6dbd47fa4d3df83c1d8b7c332bbdb62865c7
seppowalther <seppo.walther@educorvi.de>
2019-12-06 09:37:54 +0100

    Added Content Type Hersteller

--- src/uvc/hersteller/content/hersteller.py
+++ src/uvc/hersteller/content/hersteller.py
@@ -1,61 +1,57 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from uvc.hersteller import _
+from uvc.hersteller import _
 
 
 class IHersteller(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Hersteller
+    """ 
+    Hersteller von Produkten oder Maschinen
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('hersteller.xml')
+    anschrift1 = schema.TextLine(title = _(u"Anschrift 1"),
+            description = _(u"Bitte geben Sie hier die Anschrift des Herstellers ein."),
+            required = True,)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    anschrift2 = schema.TextLine(title = _(u"Anschrift 2"),
+            description = _(u"Bitte geben Sie hier einen evtl. Adresszusatz des Herstellers ein."),
+            required = False,)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    anschrift3 = schema.TextLine(title = _(u"Anschrift 3"),
+            description = _(u"Bitte geben Sie hier einen evtl. weiteren Adresszusatz des Herstellers ein."),
+            required = False,)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    land = schema.TextLine(title = _("Land"),
+            description = _(u"Bitte geben Sie hier das Land des Herstellers ein."),
+            required = True,)
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    telefon = schema.TextLine(title = _("Telefonnummer"),
+            description = _(u"Bitte geben Sie hier die vollständige Telefonnummer mit Ländercode ein,\
+                              Beispiel: +49 (0) 30 12345/678"),
+            required = True,)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    telefax = schema.TextLine(title = _("Faxnummer"),
+            description = _(u"Bitte geben Sie hier die vollständige Telefaxnummer mit Ländercode ein,\
+                              Beispiel: +49 (0) 30 12345/777"),
+            required = False,)
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    email = schema.TextLine(title = _("E-Mail Adresse"),
+            description = _(u"Bitte geben Sie hier die E-Mailadresse des Herstellers ein."),
+            required = False,)
 
+    homepage = schema.URI(title = _("Hompage"),
+            description = _(u"Bitte geben Sie hier die Internetadresse (http://www.example.de)\
+                              des Herstellers ein."),
+            required = False)
 
 @implementer(IHersteller)
 class Hersteller(Container):
